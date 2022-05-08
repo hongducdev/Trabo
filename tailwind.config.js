b6fba851fdbc31fc9230df43e4e8dc6c6ee4318a
Nguyễn Hồng Đức <contact.hongduc@gmail.com>
2022-05-08 21:00:38 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Sora': ['Sora', 'sans-serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
